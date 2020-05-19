2e76d7e84949b2084c6387172c7d67445d89a5dc
ygomez2019crn <ygomez.crn@artsgrafiques.org>
2020-05-19 20:01:51 +0200

    remove

--- js/ranger.js
+++
@@ -1,8 +0,0 @@
-//mover input range (pantalla options)
-
-
-
-document.getElementById("tirador_distancia").addEventListener('input' ,functionRanger);
-function functionRanger(){
-  document.getElementById("rangernumber").innerHTML = tirador_distancia.value;
-}
